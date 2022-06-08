74bff671fe0507b370d461fea229e55481a5bb7f
Guy Dunton <guydunton@economist.com>
2022-06-08 09:34:05 +0100

    Add screen scrolling

--- drivers/screen.c
+++ drivers/screen.c
@@ -1,17 +1,18 @@
 #include "screen.h"
+#include "../kernal/util.h"
 #include "ports.h"
 
 int get_cursor_offset();
 void set_cursor_offset(int offset);
 int print_char(char character, int col, int row, char attribute_byte);
-int get_screen_offset(int col, int row);
+int get_offset(int col, int row);
 int get_offset_row(int offset);
 int get_offset_col(int offset);
 
 void kprint_at(char* message, int col, int row) {
   int offset;
   if (col >= 0 && row >= 0) {
-    offset = get_screen_offset(col, row);
+    offset = get_offset(col, row);
   } else {
     offset = get_cursor_offset();
     row = get_offset_row(offset);
@@ -39,7 +40,7 @@ void clear_screen() {
     screen[i * 2] = ' ';
     screen[i * 2 + 1] = WHITE_ON_BLACK;
   }
-  set_cursor_offset(get_screen_offset(0, 0));
+  set_cursor_offset(get_offset(0, 0));
 }
 
 int print_char(char character, int col, int row, char attribute_byte) {
@@ -54,13 +55,13 @@ int print_char(char character, int col, int row, char attribute_byte) {
   if (col >= MAX_COLS || row >= MAX_ROWS) {
     vidmem[2 * (MAX_COLS) * (MAX_ROWS)-2] = 'E';
     vidmem[2 * (MAX_COLS) * (MAX_ROWS)-1] = RED_ON_WHITE;
-    return get_screen_offset(col, row);
+    return get_offset(col, row);
   }
 
   // Get the video memory offset for the screen location
   int offset;
   if (col >= 0 && row >= 0) {
-    offset = get_screen_offset(col, row);
+    offset = get_offset(col, row);
   } else {
     // Otherwise use the current cursor position
     offset = get_cursor_offset();
@@ -71,7 +72,7 @@ int print_char(char character, int col, int row, char attribute_byte) {
   // the next row
   if (character == '\n') {
     row = get_offset_row(offset);
-    offset = get_screen_offset(0, row + 1);
+    offset = get_offset(0, row + 1);
   } else {
     // Otherwise write the character and it's attribute byte to
     // video memory at our calculated offset
@@ -80,9 +81,23 @@ int print_char(char character, int col, int row, char attribute_byte) {
     offset += 2;
   }
 
-  // Make scrolling adjustment, fo when we reach the bottom
-  // of the screen
-  // offset = handle_scrolling(offset);
+  // Check if the offset is over screen size & scroll
+  if (offset >= MAX_ROWS * MAX_COLS * 2) {
+    for (int i = 1; i < MAX_ROWS; i++) {
+      char* start = (unsigned char*)(get_offset(0, i) + VIDEO_ADDRESS);
+      char* end = (unsigned char*)(get_offset(0, i - 1) + VIDEO_ADDRESS);
+      memory_copy(start, end, MAX_COLS * 2);
+    }
+
+    // Blank last line
+    char* last_line =
+        (unsigned char*)(get_offset(0, MAX_ROWS - 1) + VIDEO_ADDRESS);
+    for (int i = 0; i < MAX_COLS * 2; ++i) {
+      last_line[i] = 0;
+    }
+
+    offset -= 2 * MAX_COLS;
+  }
 
   // Update the cursor position on the screen device
   set_cursor_offset(offset);
@@ -94,6 +109,7 @@ int get_cursor_offset() {
   int offset = read_port_byte(REG_SCREEN_DATA) << 8;
   write_port_byte(REG_SCREEN_CTRL, 15);
   offset += read_port_byte(REG_SCREEN_DATA);
+  return offset * 2;
 }
 
 void set_cursor_offset(int offset) {
@@ -104,7 +120,7 @@ void set_cursor_offset(int offset) {
   write_port_byte(REG_SCREEN_DATA, (unsigned char)(offset & 0xff));
 }
 
-int get_screen_offset(int col, int row) {
+int get_offset(int col, int row) {
   return 2 * (row * MAX_COLS + col);
 }
 
